ea080634ea313f5911c4baa866c6b7b867c1b723
Lassi <csharplassi@posteo.de>
2017-10-16 16:40:32 +0200

    First Commit

---
+++ BigWorldGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BigWorldGame
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
